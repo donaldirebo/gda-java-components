fe35c5721cf6cbc974784d3cad7043b2123ad3c7
Donald4u <donaldirebo@gmail.com>
2025-12-10 19:28:03 -0500

    Project: Accelerometer tilt detection support with cloud integration
    - Fixed SensorData.java to properly store and retrieve sensor values
    - Added accelerometer sensor type constants to ConfigConst.java
    - Extended GatewayDeviceApp run duration for integration testing
    - Supports CDA accelerometer data forwarding to Ubidots cloud

--- src/main/java/programmingtheiot/data/SensorData.java
+++ src/main/java/programmingtheiot/data/SensorData.java
@@ -2,30 +2,19 @@
  * This class is part of the Programming the Internet of Things
  * project, and is available via the MIT License, which can be
  * found in the LICENSE file at the top level of this repository.
- * 
- * You may find it more helpful to your design to adjust the
- * functionality, constants and interfaces (if there are any)
- * provided within in order to meet the needs of your specific
- * Programming the Internet of Things project.
  */
-
 package programmingtheiot.data;
 
 import java.io.Serializable;
-
 import programmingtheiot.common.ConfigConst;
 
 /**
- * Shell representation of class for student implementation.
- *
+ * SensorData class for storing sensor telemetry data.
  */
 public class SensorData extends BaseIotData implements Serializable
 {
-	// static
-	
-	
 	// private var's
-	
+	private float value = ConfigConst.DEFAULT_VAL;
     
 	// constructors
 	
@@ -37,23 +26,23 @@ public class SensorData extends BaseIotData implements Serializable
 	public SensorData(int sensorType)
 	{
 		super();
+		this.setTypeID(sensorType);
 	}
-	
 	
 	// public methods
 	
 	public float getValue()
 	{
-		return 0.0f;
+		return this.value;
 	}
 	
 	public void setValue(float val)
 	{
+		this.value = val;
 	}
 	
 	/**
-	 * Returns a string representation of this instance. This will invoke the base class
-	 * {@link #toString()} method, then append the output from this call.
+	 * Returns a string representation of this instance.
 	 * 
 	 * @return String The string representing this instance, returned in CSV 'key=value' format.
 	 */
@@ -67,14 +56,13 @@ public class SensorData extends BaseIotData implements Serializable
 		return sb.toString();
 	}
 	
-	
 	// protected methods
 	
-	/* (non-Javadoc)
-	 * @see programmingtheiot.data.BaseIotData#handleUpdateData(programmingtheiot.data.BaseIotData)
-	 */
 	protected void handleUpdateData(BaseIotData data)
 	{
+		if (data instanceof SensorData) {
+			SensorData sData = (SensorData) data;
+			this.setValue(sData.getValue());
+		}
 	}
-	
 }
